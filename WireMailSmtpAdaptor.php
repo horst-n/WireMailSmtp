b74d3755333f59514fed17567b36d7eb1e73f1cd
horst-n <info@nogajski.de>
2016-10-08 11:20:16 +0200

    updated license

--- WireMailSmtpAdaptor.php
+++ WireMailSmtpAdaptor.php
@@ -2,10 +2,11 @@
 /*******************************************************************************
   *  WireMailSmtp
   *
-  *  @version     -   '0.1.9'
-  *  @date        -   $Date: 2016/02/15 $
+  *  @version     -   '0.1.10'
+  *  @date        -   2016/10/08
   *  @author      -   Horst Nogajski
   *  @licence     -   GNU GPL v2 - http://www.gnu.org/licenses/gpl-2.0.html
+  *  @licence     -   MIT - https://processwire.com/about/license/mit/
 ********************************************************************************/
 
 
@@ -50,7 +51,7 @@ class hnsmtp {
 																	   receive Messages                             */
 
 	private $smtp_certificate              = false;                 // @flydev: https://processwire.com/talk/topic/5704-wiremailsmtp/page-5#entry113290
-	
+
 	private $aValidVars                    = null;
 	private $emailMessage                  = null;
 	private $connected                     = null;
